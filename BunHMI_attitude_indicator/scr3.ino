d41dbb8330d4c0a2806f8a0f843c2a2cf1e009e3
mingchi21 <mingchi21@gmail.com>
2023-01-18 08:31:42 +0800

    ADJ: comment

--- BunHMI_attitude_indicator/scr3.ino
+++ BunHMI_attitude_indicator/scr3.ino
@@ -15,7 +15,10 @@ void scr3_update() {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-  // Pitch angle
+  // Pitch angle, 
+  // 0 is upper bound of the scale of pitch image
+  // -53 is lower bound of the scale of pitch image
+  // please refer BunMaker image setting
   int pitch_offset_y = map(a.acceleration.x, -5, 5, -53, 0);
   if (pitch_offset_y > 0) {
     pitch_offset_y = 0;
@@ -32,6 +35,7 @@ void scr3_update() {
   if (rotation_angle > 800) {
     rotation_angle = 800;
   }
+  // Due to rotation take heavy MCU time, we interleave offset and angle to make animate more smooth
   if(++interleave_cnt & 1){
     HMI_CMD("pitch.offset_y(%d)",  pitch_offset_y);
   }else{
